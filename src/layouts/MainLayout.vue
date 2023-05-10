a208b78b6a633dcd980038894f371e3db7c49748
Vlad Stan <stan.v.vlad@gmail.com>
2023-05-10 15:41:16 +0300

    chore: clean-up

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,16 +12,14 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Nostr Relay Tester
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
-      show-if-above
       bordered
       content-class="bg-grey-1"
     >
@@ -30,7 +28,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          Settings
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
@@ -49,50 +47,7 @@
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
 
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+const linksData = []
 
 export default {
   name: 'MainLayout',
